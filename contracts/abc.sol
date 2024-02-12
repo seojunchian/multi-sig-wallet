975bed7c7a3734bb3ab2fffd4c7e6ccca62400a7
seojunchian <seojunchian@gmail.com>
2024-02-12 17:15:10 +0300

    a

--- contracts/abc.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.22;
-
-contract abc {
-  string public greet = "a";
-  function get() public view returns (string memory) {
-    return greet;
-  }
-}
